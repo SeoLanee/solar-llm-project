880eec1f5a314218da8345bd8f2edc17b9124ebc
kyuhho <rbgh0114@naver.com>
2024-11-02 12:32:59 +0900

    chore: remove unusing comment (front)

--- front-end/src/components/shared/footer.tsx
+++ front-end/src/components/shared/footer.tsx
@@ -2,9 +2,8 @@ import { Textarea } from "@/components/ui/textarea";
 
 const Footer = () => {
   return (
-    // <footer className="">
     <footer className="fixed bottom-0 left-0 w-full p-4 shadow-md flex justify-center">
-      <div className="w-[64rem]">
+      <div className="w-[48rem]">
         <Textarea />
       </div>
     </footer>
