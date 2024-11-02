a6b073b38f0d7365396818683687b72565e7bb91
kyuhho <rbgh0114@naver.com>
2024-11-02 11:11:43 +0900

    feat: init project

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import { MainLayout } from "@/main-layout";
+import "@/index.css";
+import "@/styles/font.css";
+import "@/styles/reset.css";
+
+function App() {
+  return (
+    <MainLayout>
+      <div>gweagew</div>
+    </MainLayout>
+  );
+}
+
+export default App;
---
+++ front-end/src/main-layout.tsx
@@ -0,0 +1,11 @@
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <div className="flex justify-center w-full">
+      <div className="w-[64rem]">{children}</div>
+    </div>
+  );
+};
