81379c447262b3edfabbaef88854f1b8b7be90ce
kyuhho <rbgh0114@naver.com>
2024-11-02 14:23:15 +0900

    chore: remove baseInstance options (front)

--- front-end/src/apis/axios.ts
+++ front-end/src/apis/axios.ts
@@ -4,6 +4,4 @@ const baseURL = import.meta.env.VITE_BASE_URL;
 
 export const baseInstance = axios.create({
   baseURL: baseURL,
-  timeout: 5000,
-  headers: { "Content-Type": "application/json" },
 });
